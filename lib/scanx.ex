9699b393e4bddab45303d1bd88c634e88d1d4915
RobertDober <robert.dober@gmail.com>
2019-03-08 07:54:31 +0100

    initial local commit

---
+++ lib/scanx.ex
@@ -0,0 +1,18 @@
+defmodule Scanx do
+  @moduledoc """
+  Documentation for Scanx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scanx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/scanx_test.exs
@@ -0,0 +1,8 @@
+defmodule ScanxTest do
+  use ExUnit.Case
+  doctest Scanx
+
+  test "greets the world" do
+    assert Scanx.hello() == :world
+  end
+end
